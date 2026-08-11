8b619408f814fd1dea274d0838bc5b17f47af977
mana001 <72500541+mana001@users.noreply.github.com>
2026-08-11 02:02:51 +0100

    Update OWNER_ID to the actual user ID

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const BOT_TOKEN = process.env.DISCORD_TOKEN || "PASTE_YOUR_BOT_TOKEN_HERE";
 // -------------------------------------------------------------
 // 👑 PERMISSIONS CONFIGURATION
 // -------------------------------------------------------------
-const OWNER_ID = "YOUR_NUMERIC_DISCORD_USER_ID";
+const OWNER_ID = "726748866023784490";
 const BIRTHDAY_BOY_ID = "1363453807949779064";
 const REQUIRED_ROLE_NAME = "HOST B";
 
